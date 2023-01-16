c7b1705fbe2e23c2cd9ea67c4e25102d763f4669
Kimkihyun <kimghn1@naver.com>
2023-01-16 16:31:25 +0900

    task 6.2

--- Task.py
+++ Task.py
@@ -14,16 +14,30 @@
 # print(letter.format('Mr','kim','electric light','explode','room','room','cats','amount of damage',"electric light",'100','explode',
 #                     'Hans','manger'))
 
-#Task 5.6~5.8
-# % formatting
-print('%sy Mc%sface' %('Duck','Duck') )
-print('%sy Mc%sfae' %('gourd','gourd'))
-print('%sy Mc%sfae' %('Spitz','Spitz'))
-#format()
-print('{}y Mc{}face'.format('Duck','Duck') )
-print('{}y Mc{}fae' .format('gourd','gourd'))
-print('{}y Mc{}fae'.format('Spitz','Spitz'))
-#f-formating
-for i in ['Ducky','Gourd','Spitz']:
-    print(f'{i}y Mc{i}face')
+# #Task 5.6~5.8
+# # % formatting
+# print('%sy Mc%sface' %('Duck','Duck') )
+# print('%sy Mc%sfae' %('gourd','gourd'))
+# print('%sy Mc%sfae' %('Spitz','Spitz'))
+# #format()
+# print('{}y Mc{}face'.format('Duck','Duck') )
+# print('{}y Mc{}fae' .format('gourd','gourd'))
+# print('{}y Mc{}fae'.format('Spitz','Spitz'))
+# #f-formating
+# for i in ['Ducky','Gourd','Spitz']:
+#     print(f'{i}y Mc{i}face')
 
+#Task 6.2
+import random as rnd
+guess_me=rnd.randint(1,10)
+number=int(input('숫자를 입력하세요:'))
+while number:
+    if guess_me>number:
+        print('too low')
+        number = int(input('숫자를 입력하세요:'))
+    elif guess_me<number:
+        print('oops')
+        number = int(input('숫자를 입력하세요:'))
+    else:
+        print('found it')
+        break
