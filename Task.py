7aee148f4c1dd645d9294dcf742cd30169ba95d0
Kimkihyun <kimghn1@naver.com>
2023-01-16 16:07:12 +0900

    task 5.4

---
+++ Task.py
@@ -0,0 +1,15 @@
+#Task 5.4
+letter="""Dear {} {},
+
+Thank you for your letter. we are sorry that our {} {} in your
+{}. please note that it should never be used in a {}, especially near any
+{}.
+send us your receipt and {} for shipping and handling. We will send you another
+{} that, in our tests, is {}% less likely to have {}.
+
+Thank you for your support.
+sincerely
+{}
+{}"""
+print(letter.format('Mr','kim','electric light','explode','room','room','cats','amount of damage',"electric light",'100','explode',
+                    'Hans','manger'))
