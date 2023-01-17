32a37c66e9e6bef2a0d677bc099f71c839d77989
Kimkihyun <kimghn1@naver.com>
2023-01-17 16:13:30 +0900

    task 8.6~8.9

--- Task.py
+++ Task.py
@@ -1,11 +1,10 @@
-#8.1
-e2f={'dog':'chien','cat':'chat','walrus':'morse'}
-#8.2
-print(e2f['walrus'])
-#8.3
-f2e={v:k for k,v in e2f.items()}
-#8.4
-print(f2e['chien'])
-#8.5
-for k,v in e2f.items():
-    print(k,v)
+#8.6
+life={'animals':['cats','octopi','emus'],'plants':['Henri','Grumpy','Lucky'],'other':[]}
+#8.7
+for k in life.keys():
+    print(k, end=' ')
+#8.8
+print(life['animals'])
+#8.9
+print(life['animals'],life['plants'])
+
