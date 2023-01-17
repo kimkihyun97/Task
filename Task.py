07fa82a792593f76c71b82a4bb1504213e432556
Kimkihyun <kimghn1@naver.com>
2023-01-17 16:23:01 +0900

    task 8.10~8.12

--- Task.py
+++ Task.py
@@ -1,10 +1,14 @@
-#8.6
-life={'animals':['cats','octopi','emus'],'plants':['Henri','Grumpy','Lucky'],'other':[]}
-#8.7
-for k in life.keys():
-    print(k, end=' ')
-#8.8
-print(life['animals'])
-#8.9
-print(life['animals'],life['plants'])
-
+#8.10
+square={i:i**2 for i in range(10)}
+print(square)
+#8.11
+set_odd=set([i for i in range(10) if i%2!=0])
+print(set_odd)
+#8.12
+gen_word=(i for i in 'Got')
+gen_num=(i for i in range(10))
+def for_rotate(s):
+    for i in list(s):
+        print(i,end=' ')
+for_rotate(gen_word)
+for_rotate(gen_num)
