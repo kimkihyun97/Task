668b75298c0313061dd2069c7183d6c3445608f3
Kimkihyun <kimghn1@naver.com>
2023-01-17 14:34:28 +0900

    7.11

--- Task.py
+++ Task.py
@@ -1,3 +1,18 @@
-#7.10
-even=[i for i in range(1,11) if i%2==0]
-print(even)
+#7.11
+import random as rd
+start1=['fee','fie','foe']
+rhymes=[
+    ('flop','get a mop'),
+    ('fope','turn the rope'),
+    ('fa','get your ma'),
+    ('fudge','call the judge'),
+    ('fat','pet the car'),
+    ('fog','walk the dog'),
+    ('fun',"say we're done")
+]
+start2='someone better'
+rd_rhy=rd.randint(0,6)
+for i in start1:
+    print(f'{i.capitalize()}!',end='')
+print(f'{rhymes[rd_rhy][0].capitalize()}!')
+print(f'{start2} {rhymes[rd_rhy][1]}')
