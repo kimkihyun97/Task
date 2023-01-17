7f42691d7f0913a1fcc59cdb0ec420f2ac03453f
Kimkihyun <kimghn1@naver.com>
2023-01-17 14:18:16 +0900

    task 7.10

--- Task.py
+++ Task.py
@@ -1,6 +1,3 @@
-#7.8
-surpirse=['Groucho','Chico','Harpo']
-#7.9
-surpirse_last_word=[*surpirse[:2],surpirse[2].lower()]
-surpirse_first_word=[surpirse[-1].upper(),*surpirse[-2::-1]]
-print(surpirse_last_word,surpirse_first_word)
+#7.10
+even=[i for i in range(1,11) if i%2==0]
+print(even)
