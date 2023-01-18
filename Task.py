545938dd0af2a7fd07666eb10b85c0241c3b953c
Kimkihyun <kimghn1@naver.com>
2023-01-18 16:09:33 +0900

    Task9.16_9.1~9.2

--- Task.py
+++ Task.py
@@ -1,5 +1,13 @@
-#8.14
-title=['Creature of Habit','Crewel Fate']
-plots=['A nun','A haunted yarn shop']
-movie={t:p for t,p in zip(title,plots)}
+#9.16
+#9.1
+def get_list():
+    print(['Harry','Ron','Hermione'])
+
+#9.2
+def get_odds():
+    return [i for i in range(10) if i%2!=0]
+
+
+
+
 
