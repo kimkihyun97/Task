164bd992d96dd09b0b71b51094b89b55604d3a93
Kimkihyun <kimghn1@naver.com>
2023-01-18 15:58:00 +0900

    task 8.14

--- Task.py
+++ Task.py
@@ -1,9 +1,5 @@
-#8.13
-key_candid=('optimist','pessiist','troll')
-value_candid=('The glass is half full','The glass is half empty','How did you get a glass')
-dic_zip={x:y for x,y in zip(key_candid,value_candid)}
 #8.14
 title=['Creature of Habit','Crewel Fate']
-plots=['A num turns into a monster','A haunted yarn shop']
-movies={x:y for x,y in zip(title,plots)}
-print(movies)
+plots=['A nun','A haunted yarn shop']
+movie={t:p for t,p in zip(title,plots)}
+
