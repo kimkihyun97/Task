185d1ef46c071a4159ce93a74224c794a29a25f6
Kimkihyun <kimghn1@naver.com>
2023-01-19 16:13:31 +0900

    task 9.16 9.3

--- Task.py
+++ Task.py
@@ -1,13 +1,13 @@
-#9.16
-#9.1
-def get_list():
-    print(['Harry','Ron','Hermione'])
+##9.16
+def test_deco(func):
+    try:
+        print('start')
+        print(func())
+    finally:
+        print('end')
 
-#9.2
+@test_deco
 def get_odds():
     return [i for i in range(10) if i%2!=0]
 
-
-
-
-
+get_odds
