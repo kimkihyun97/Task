b78780a8fbc4dd7612eede131e9456d100b69cb2
Kimkihyun <kimghn1@naver.com>
2023-01-20 16:21:09 +0900

    task 10.4~10.7

--- Task.py
+++ Task.py
@@ -1,7 +1,23 @@
-#10.3
-class Thing3():
-    def __init__(self,letter):
-        self.letter = letter
+#10.4
+class Element():
+    def __init__(self,name,symbol,number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
     def __str__(self):
-        return self.letter
-print(Thing3('xyz')) #객체 필요 없음
+        return f'{self.name}, {self.symbol}, {self.number}'
+
+    def dump(self):
+        print(f'{self.name}, {self.symbol}, {self.number}')
+obj=Element('hydrogen','h',1)
+
+#10.5
+el_dict={'name':'Hydrogen','symbol':'H','number':1}
+print(Element(el_dict['name'],el_dict['symbol'],el_dict['number']))
+
+#10.6
+hydrogen=Element('hydrogen','h',1)
+hydrogen.dump()
+
+#10.7
+print(hydrogen)
