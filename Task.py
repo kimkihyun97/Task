61f3b9e0df200e7cb788ccb6808ba54cf7b5e36e
Kimkihyun <kimghn1@naver.com>
2023-01-20 16:08:49 +0900

    task 10.3

--- Task.py
+++ Task.py
@@ -1,9 +1,7 @@
-#10.2
-class Thing2():
+#10.3
+class Thing3():
     def __init__(self,letter):
         self.letter = letter
     def __str__(self):
         return self.letter
-
-ex=Thing2('abc')
-print(ex)
+print(Thing3('xyz')) #객체 필요 없음
