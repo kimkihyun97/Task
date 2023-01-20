65681c23047e16cd1cfa6221f42e5bf1c0225012
Kimkihyun <kimghn1@naver.com>
2023-01-20 16:03:25 +0900

    task 10.1

--- Task.py
+++ Task.py
@@ -1,18 +1,7 @@
-##9.16
-##9.4
-class OopsException(Exception):
-    def __init__(self,msg):
-        self.msg=msg
-    def __str__(self):
-        return self.msg
+#10.1
+class Thing():
+    pass
 
-def putName():
-    name=input('이름을 입력해주세요: ')
-    if len(name)<3:
-        raise OopsException('Caught an oops')
-    print(name)
-
-try:
-    putName()
-except OopsException as oop:
-    print(oop)
+print(Thing)
+example=Thing()
+print(example)
