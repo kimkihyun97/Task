dbb6796fdfa09a40ac5a9e5717127dcbeafd1a48
Kimkihyun <kimghn1@naver.com>
2023-02-06 17:34:46 +0900

    Chap 06 store_distance

--- Task.py
+++ Task.py
@@ -1,27 +1,26 @@
-#10.9
-class Bear():
-    def eat(self):
-        return 'berries'
+import random
+import math
+import string
+class Node( ):
+    def __init__(self,name,x,y):
+        self.name = name
+        self.x = x
+        self. y = y
 
-class Rabbit():
-    def eat(self):
-        return 'carrot'
+    def get_distance(self):
+        return math.sqrt(self.x**2+self.y**2)
 
-class Octothorpe():
-    def eat(self):
-        return 'campers'
+    def get_profile(self):
+        print(f'{self.name}, distance : {math.sqrt(self.x**2+self.y**2)}')
 
-#10.10
-class Laser():
-    def does(self):
-        return 'disintegrate'
+def get_store(name, x, y):
+    return Node(name,x,y)
 
+if __name__ == '__main__' :
 
-class Calw():
-    def does(self):
-        return 'crush'
+    store =[get_store(i,random.randint(1,100),random.randint(1,100)) for i in string.ascii_lowercase[:11]]
+    store_distance = [i.get_distance() for i in store]
+    store_idx_distance_sort = sorted([[i,d] for i,d in enumerate(store_distance)], key = lambda x : x[1])
+    for idx, d in store_idx_distance_sort :
+        store[idx].get_profile()
 
-
-class Smartphone():
-    def does(self):
-        return 'ring'
